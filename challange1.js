94d4e2c7d1918092e6dd7124322e76df1dce9657
Ritu Patel <9ritspatel5gmail.com>
2020-01-10 13:11:09 +0530

    Test Commit

---
+++ challange1.js
@@ -0,0 +1,12 @@
+var no=[1,2,3,4];
+var name = ['Ritu','Shubham','Vir','Kunjan'];
+// console.log(no);
+// console.log(name);
+
+document.getElementById('number').innerHTML=no ;
+// console.log(name.length);
+
+// for (var i = 0;i <= no.length &&  i<=name.length; i++) {
+
+// 	console.log(name);
+// }	
